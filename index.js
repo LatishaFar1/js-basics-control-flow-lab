2ffabd41fa1a6cc6960ebb1a941b0345521593df
Latisha Faroul <Lfaroul14@gmail.com>
2021-05-18 19:45:29 -0400

    commit tree 60b10661e4abb8ddcb4d6672266863e5c386d021

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(scuberFeet){
   // Write your code here!
+   
+  if (scuberFeet <=400){
+    return("This one is on me!");
+  }
+  else if (scuberFeet > 2000 && scuberFeet< 2500){
+    return("I will gladly take your thirty bucks.");
+  }
+  else if (scuberFeet > 2500){
+    return ("No can do.");
+  }
+  
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return ("Thank you so much.");
+    case 'not as generous':
+      return ("Thank you.");
+      default:
+      return ("Bye.");    
+  }
 }
